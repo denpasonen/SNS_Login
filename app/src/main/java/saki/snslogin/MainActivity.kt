6fc05768e829316866d2552ffdf26bf4601bdb32
saki <denpasonen@hanmail.net>
2025-06-10 13:10:20 +0900

    kakaoLogin Design modify

--- app/src/main/java/saki/snslogin/MainActivity.kt
+++ app/src/main/java/saki/snslogin/MainActivity.kt
@@ -7,9 +7,13 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -17,7 +21,9 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import saki.snslogin.ui.theme.SNSLoginTheme
 
 class MainActivity : ComponentActivity() {
@@ -37,6 +43,7 @@ class MainActivity : ComponentActivity() {
                             modifier = Modifier
                                 .fillMaxSize()
                                 .padding(it)
+                                .padding(20.dp)
                         ) {
                             initBtn()
                         }
@@ -50,14 +57,19 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun initBtn() {
     Column() { kakaoLoginBtn() }
-
 }
 
 @Composable
 fun kakaoLoginBtn() {
     val context = LocalContext.current
     Button(
-        onClick = { snsType("kakao", context) }
+        onClick = { snsType("kakao", context) },
+        contentPadding = PaddingValues(12.dp),
+        modifier = Modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(10.dp),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = colorResource(id = R.color.kakao_btn),
+            contentColor = colorResource(id = R.color.black))
     ) {
         Text(text = stringResource(id = R.string.kakaoLogin))
     }
